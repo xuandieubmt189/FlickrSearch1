e4754838519d3751d5e1da7eb0e5f5816eb28701
xuandieu <xuandieu@xuandieus-Mac.local>
2017-04-28 20:36:00 -0700

    new commit

---
+++ FlickrSearch/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by xuandieu on 4/28/17.
+//  Copyright © 2017 xuandieu. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
